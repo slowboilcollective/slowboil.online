2fd07512e1ffc97d34c3af048d350c13fa5cb4a2
Lachlan Kermode <lachiekermode@gmail.com>
2021-05-24 20:02:55 +1200

    Initial pass towards new designs

---
+++ index.js
@@ -0,0 +1,54 @@
+  $('.navbar-container').hide()
+  $('.subnavbar-container').hide()
+
+function update_nav() {
+  const y = $(this).scrollTop()
+  if (y > 700) {
+    $('.navbar-container').fadeIn()
+    // $('.subnavbar-container').fadeIn()
+  } else {
+    $('.navbar-container').fadeOut()
+    // $('.subnavbar-container').fadeOut()
+  }
+}
+
+$(document).scroll(function() {
+  update_nav()
+})
+
+/* ROUTING */
+let PATHNAME = window.location.pathname
+function update() {
+  PATHNAME = window.location.pathname
+}
+function goToPath(pth) {
+  window.history.pushState({}, "", pth)
+}
+function scrollTo(elId) {
+  const el = document.getElementById(elId)
+  const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+  if (width < 1080) {
+
+    const rect = el.getBoundingClientRect()
+    window.scrollTo(0, rect.top - 60)
+
+  }
+  // else {
+  //   el.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"})
+  // }
+}
+
+window.clickHandler = function clickHandler(pth) {
+  goToPath(pth)
+  // scrollTo(`${pth.split('/')[1]}-label`)
+}
+
+
+// go to path if given
+if (PATHNAME !== "/") {
+  const id = PATHNAME.split("/")[1]
+  document.getElementById(id).checked = true
+  scrollTo(`${id}-label`)
+}
+
+
