78f95de20dfe894ebed17b445bfad71fcbb86a65
Lachlan Kermode <lachiekermode@gmail.com>
2021-06-12 22:18:49 +1200

    Add workshops

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ $(document).scroll(function() {
 let PATHNAME = window.location.pathname
 
 function goTo(pth) {
-  $('#mobile-navbar').toggle();
+  $('#mobile-navbar').hide();
   window.history.pushState({}, "", pth)
   window.scrollTo(0,0)
   $('.page').map(function() {
@@ -30,6 +30,33 @@ function goTo(pth) {
     } else {
       $(this).hide()
     }
+  })
+  if (window.location.pathname === "/workshops") { loadIframe() }
+  else { $("body").css("overflow", "auto") }
+}
+
+function loadIframe() {
+  const content = '#workshop-content'
+  const loading = '#workshop-loading'
+  const iframe = '#workshop-iframe'
+  const source = 'https://docs.google.com/document/d/e/2PACX-1vSs3c3MrLNCt3WoWh2PjkgNFeJpCnGr4JnMxsDMm_cFcjg1NlSCOF90Q_CIj4-fuiy2evEAMqeGtKBo/pub?embedded=true'
+
+  $("body").css("overflow", "hidden");
+  $.get(source, (data, status) => {
+    $(iframe).contents().find('head').append('<base target="_blank">')
+    const modData = data.replace(
+      /href="(https\:\/\/www\.google\.com\/url\?q\=)(.*?)(\&)([^\'\"]+)(\")/g,
+      (match, p1, p2) => {
+        p2 = p2.replace('%23', '#')
+        return `href="${p2}"`
+      }
+    )
+    console.log(modData)
+    $(iframe).contents().find("body").html('');
+    $(iframe).contents().find("body").append(modData)
+    $(loading).hide()
+    $(content).css('display', 'flex')
+    $(iframe).contents().find("body").append('<style>p, span { color: rgb(34, 39, 90)!important; font-size: 34pt!important; padding-top: 30px!important; } a { color: var(--dark-blue)!important; } table { width: 100%; margin: auto; } .title > span { font-size: 45pt!important; font-weight: bold; }</style>')
   })
 }
 
@@ -68,3 +95,5 @@ if (is_mobile) {
   $('.subnavbar-container').hide()
   $('#mobile-navbar').hide()
 }
+
+if (window.location.pathname === "/workshops") loadIframe()
